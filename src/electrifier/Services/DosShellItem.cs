6eb2a673b6cb1e4e624e4fe30841cc29b39a4b5f
Thorsten Jung <tajbender@hotmail.de>
2024-02-17 19:14:22 +0100

    gui: FileManagerPage, shows folder content with default icons

--- src/electrifier/Services/DosShellItem.cs
+++ src/electrifier/Services/DosShellItem.cs
@@ -1,39 +1,45 @@
 ﻿//using muxc = Microsoft.UI.Xaml.Controls;
 using System.ComponentModel;
+using System.Resources;
 using System.Runtime.CompilerServices;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media.Imaging;
 using Windows.Storage;
+using Windows.Storage.FileProperties;
 
 namespace electrifier.Services;
 
 public class DosShellItem : INotifyPropertyChanged
 {
-    public IStorageItem StorageItem
-    {
-        get;
-    }
+    private const string shell32DefaultUnknownFileIcon = "ms-appx:///Assets /Views/Workbench/Shell32 Default unknown File.ico";
 
     public string FileName => StorageItem.Name;
 
     public string FileType => StorageItem.Path;
 
+    public bool IsFile => !IsFolder;
     public bool IsFolder
     {
         get;
     }
-
-    public bool IsFile => !IsFolder;
-
-    //public ImageIcon ShellIcon
-    //{
-    //    get;        // TODO: this is not initialized yet
-    //}
+    public ImageIcon ShellIcon
+    {
+        get;        // TODO: this is not initialized yet
+    }
+    public IStorageItem StorageItem
+    {
+        get;
+    }
 
     public DosShellItem(IStorageItem storageItem)
     {
         StorageItem = storageItem;
         IsFolder = storageItem.IsOfType(StorageItemTypes.Folder);
 
-        //_ = LoadDefaultImages();
+        ShellIcon = new ImageIcon();
+
+            //ResourceManager.GetForCurrentView().GetString("Files/foo.png");
+        // shell32DefaultUnknownFileIcon
 
 
         /////////////////////////////////////////////////////////////
@@ -115,44 +121,35 @@ public class DosShellItem : INotifyPropertyChanged
 
 
 
-    //    public async Task<BitmapImage> GetImageThumbnailAsync()
-    //    {
-    //        try
-    //        {
-    //            var bitmapImage = new BitmapImage();
+    public async Task<BitmapImage> GetImageThumbnailAsync()
+    {
+        try
+        {
+            var bitmapImage = new BitmapImage();
 
-    //            // TODO: Load folder image from resource
-    //            if (IsFolder)
-    //            {
+            // TODO: Load folder image from resource
+            if (IsFolder)
+            {
+                //// "..\Assets\Views\Workbench\Shell32 Default unknown File.ico"
+                //bitmapImage.SetSourceAsync();
+            }
+            else
+            {
+                using var thumbnail = await ((StorageFile)StorageItem).GetThumbnailAsync(ThumbnailMode.SingleItem);
+                if (thumbnail != null)
+                {
+                    bitmapImage.SetSource(thumbnail);
+                    thumbnail.Dispose();
+                }
+            }
 
-    //                    // "..\Assets\Views\Workbench\Shell32 Default unknown File.ico"
-    ////                bitmapImage.SetSourceAsync(bitmapImage)
-    //            }
-    //            else
-    //            {
-    //                //using (var thumbnail = await StorageFile.GetThumbnailAsync(ThumbnailMode.SingleItem))
-    //                //{
-    //                //    bitmapImage.SetSource(thumbnail);
-    //                //}
-    //            }
-
-    //            //using (var thumbnail = await StorageItem.GetThumbnailAsync(ThumbnailMode.SingleItem))
-    //            //{
-    //            //    // Create a bitmap to be the image source.
-    //            //    if (thumbnail != null)
-    //            //    {
-    //            //        bitmapImage.SetSource(thumbnail);
-    //            //        thumbnail.Dispose();
-    //            //    }
-    //            //}
-
-    //            return bitmapImage;
-    //        }
-    //        catch (Exception)
-    //        {
-    //            throw;
-    //        }
-    //    }
+            return bitmapImage;
+        }
+        catch (Exception)
+        {
+            throw;
+        }
+    }
 
     public event PropertyChangedEventHandler? PropertyChanged;
 
--- src/electrifier/Views/FileManagerPage.xaml.cs
+++ src/electrifier/Views/FileManagerPage.xaml.cs
@@ -3,30 +3,53 @@ using electrifier.ViewModels;
 using Microsoft.UI.Xaml.Controls;
 using System.Collections.ObjectModel;
 using Windows.Storage;
-using Microsoft.UI.Xaml.Media.Imaging;
+//using Microsoft.UI.Xaml.Media.Imaging;
+//using CommunityToolkit.WinUI.Collections;
+using CommunityToolkit.WinUI.UI;
+using System;
+using System.Diagnostics;
 
 namespace electrifier.Views;
 
 public sealed partial class FileManagerPage : Page
 {
-    public FileManagerViewModel ViewModel
+    public AdvancedCollectionView CollectionView
     {
         get;
     }
     public ObservableCollection<DosShellItem> ShellItems { get; } = new ObservableCollection<DosShellItem>();
+    public FileManagerViewModel ViewModel
+    {
+        get;
+    }
+
 
     public FileManagerPage()
     {
         ViewModel = App.GetService<FileManagerViewModel>() ?? throw new InvalidOperationException();
 
+        // Set up the AdvancedCollectionView with live shaping enabled to filter and sort the original list
+        CollectionView = new AdvancedCollectionView(ShellItems, true);
+        // And sort ascending by the property "FileName"
+        CollectionView.SortDescriptions.Add(new SortDescription("FileName", SortDirection.Ascending));
+
         InitializeComponent();
+        // AdvancedCollectionView can be bound to anything that uses collections. 
+        ImageGridView.ItemsSource = CollectionView;
+
+        _ = GetItemsAsync(KnownLibraryId.Pictures);
+
+
+        // Let's filter out the integers
+        // Let's add a Person to the observable collection
+        //        var person = new DosShellItem { Name = "Aardvark" };
+        //        ShellItems.Add(person);
+        // Our added person is now at the top of the list, but if we rename this person, we can trigger a re-sort
+        //        person.Name = "Zaphod"; // Now a re-sort is triggered and person will be last in the list
 
         // StorageFolder storageFolder = Package.Current.InstalledLocation;
         // StorageLibrary storageFolder = await StorageLibrary.GetLibraryAsync(KnownLibraryId.Pictures);
         // var library = await StorageLibrary.GetLibraryAsync(KnownLibraryId.Pictures);
-
-        _ = GetItemsAsync(KnownLibraryId.Pictures);
-        //_ = GetItemsAsync(storageFolder);
     }
 
     private void ImageGridView_ContainerContentChanging(
@@ -64,50 +87,42 @@ public sealed partial class FileManagerPage : Page
 
             if (templateRoot != null)
             {
-                //var imageElement = templateRoot.FindName("ItemImage") as Image;
                 var imageElement = templateRoot.FindName("ItemImageIcon") as ImageIcon;
 
-//                if (imageElement != null)
-//                {
-//                    if (args.Item is DosShellItem item)
-//                    {
-//                        //var bitmap = new BitmapImage
-//                        //{
-//                        //    //imageElement.Source = new BitmapImage();
-//                        //    //                        bitmap.UriSource = new Uri("ms-appx:///Assets/Square44x44Logo.scale-200.png");
-//                        //    UriSource = new Uri("ms-appx:///../Assets/Square44x44Logo.scale-200.png")
-//                        //};
+                if (imageElement != null)
+                {
+                    if (args.Item is DosShellItem item)
+                    {
+                        var bitmapImage = await item.GetImageThumbnailAsync();
+                        imageElement.Source = bitmapImage;
 
 
-//                        //new Uri(img.BaseUri, "Assets/StoreLogo.png");
-//                        //img.Source = bitmapImage;
 
+                        /*
+                         *     Image img = sender as Image; 
+                //BitmapImage bitmapImage = new BitmapImage();
+                //img.Width = bitmapImage.DecodePixelWidth = 80; 
+                //// Natural px width of image source.
+                //// You don't need to set Height; the system maintains aspect ratio, and calculates the other
+                //// dimension, as long as one dimension measurement is provided.
+                //bitmapImage.UriSource = new Uri(img.BaseUri,"Assets/StoreLogo.png");
+                //img.Source = bitmapImage;*/
 
-//                        /*
-//                         *     Image img = sender as Image; 
-//BitmapImage bitmapImage = new BitmapImage();
-//img.Width = bitmapImage.DecodePixelWidth = 80; 
-//// Natural px width of image source.
-//// You don't need to set Height; the system maintains aspect ratio, and calculates the other
-//// dimension, as long as one dimension measurement is provided.
-//bitmapImage.UriSource = new Uri(img.BaseUri,"Assets/StoreLogo.png");
-//img.Source = bitmapImage;*/
+                        //imageElement.Source = new ImageSource(item.ShellIcon);
 
-//                        //imageElement.Source = new ImageSource(item.ShellIcon);
+                        //var task = item?.GetImageThumbnailAsync();
 
-//                        //var task = item?.GetImageThumbnailAsync();
+                        //if (task != null)
+                        //{
+                        //    imageElement.Source = await task;
+                        //}
 
-//                        //if (task != null)
-//                        //{
-//                        //    imageElement.Source = await task;
-//                        //}
-
-//                    }
-//                    else
-//                    {
-//                        throw new ArgumentNullException(nameof(args.Item));
-//                    }
-//                }
+                    }
+                    else
+                    {
+                        throw new ArgumentNullException(nameof(args.Item));
+                    }
+                }
             }
         }
     }
@@ -126,15 +141,14 @@ public sealed partial class FileManagerPage : Page
     private async Task GetItemsAsync(StorageFolder storageFolder)
     {
         var folderQuery = storageFolder.CreateItemQuery();
-
         var items = await folderQuery.GetItemsAsync();
 
-        foreach (var storageFile in items)
+        foreach (var storageItem in items)
         {
-            ShellItems.Add(await LoadShellItemInfo(storageFile));
+            ShellItems.Add(await LoadShellItemInfo(storageItem));
         }
 
-        ImageGridView.ItemsSource = ShellItems;
+        //ImageGridView.ItemsSource = ShellItems;
 
         //        var fileQuery = storageFolder.CreateFileQueryWithOptions(new QueryOptions());
         //        var storageFiles = await folderQuery.GetFilesAsync();
@@ -160,58 +174,22 @@ public sealed partial class FileManagerPage : Page
 
                 var storageFiles = await result.GetFilesAsync();
 
-                foreach (var storageFile in storageFiles)
+                foreach (var storageItem in storageFiles)
                 {
-                    ShellItems.Add(await LoadShellItemInfo(storageFile));
+                    ShellItems.Add(await LoadShellItemInfo(storageItem));
                 }
 
                 ImageGridView.ItemsSource = ShellItems;
         */
     }
 
-    //public static async Task<DosShellItem> LoadShellItemInfo(StorageFile file)
-    //{
-    //    DosShellItem shellItem = new(file ?? throw new ArgumentNullException(nameof(file)));
-
-    //    try
-    //    {
-    //        var properties = await file.Properties.GetDocumentPropertiesAsync();
-
-    //        if (file.IsOfType(StorageItemTypes.Folder))
-    //        {
-    //            bool isFolder = true;
-
-    //        }
-
-    //        return shellItem;
-    //    }
-    //    catch
-    //    {
-    //        throw;
-    //    }
-    //}
-
-
     public static async Task<DosShellItem> LoadShellItemInfo(IStorageItem item)
     {
         DosShellItem shellItem = new(item ?? throw new ArgumentNullException(nameof(item)));
+        Debug.Assert(shellItem != null);
 
-        try
-        {
-            var storageItem = item;
+        await item.GetBasicPropertiesAsync();
 
-            if (item.IsOfType(StorageItemTypes.Folder))
-            {
-                var isFolder = true;
-
-            }
-            await item.GetBasicPropertiesAsync();
-
-            return shellItem;
-        }
-        catch
-        {
-            throw;
-        }
+        return shellItem;
     }
 }
