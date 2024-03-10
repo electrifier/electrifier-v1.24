c6d0cb58da666d68b8bb63923717c6e3d41300e9
Thorsten Jung <tajbender@users.noreply.github.com>
2024-03-10 13:43:15 +0100

    project: update packages

--- src/electrifier/ViewModels/FileManagerViewModel.cs
+++ src/electrifier/ViewModels/FileManagerViewModel.cs
@@ -18,43 +18,43 @@ public partial class FileManagerViewModel : ObservableRecipient
     {
     }
 
-//    private readonly Task enumerateFoldersTask = Task.Run(() => EnumerateFolders(@"C:\", true));
-//
-//    /// <summary>
-//    /// functionality to enumerate folders
-//    /// <throws cref="DirectoryNotFoundException"></throws>
-//    /// <throws cref="UnauthorizedAccessException"></throws>
-//    /// <throws cref="PathTooLongException"></throws>
-//    /// </summary>
-//    /// 
-//    public static string EnumerateFolders(string rootPath, bool enumerateChildFoldersIfAllowed = false)
-//    {
-//        try
-//        {
-//            foreach (var dirName in Directory.EnumerateDirectories(rootPath))
-//            {
-//                return dirName;
-//                //Folders.Add(dirName);
-//
-//                //if (enumerateChildFoldersIfAllowed)
-//                //{
-//                //    EnumerateFolders(dirName, false);
-//                //}
-//            }
-//        }
-//        catch (DirectoryNotFoundException ex)
-//        {
-//            throw new DirectoryNotFoundException(rootPath, ex);
-//        }
-//        catch (UnauthorizedAccessException ex)
-//        {
-//            throw new UnauthorizedAccessException(rootPath, ex);
-//        }
-//        catch (PathTooLongException ex)
-//        {
-//            throw new PathTooLongException(rootPath, ex);
-//        }
-//
-//        return "{ unknown error in $EnumerateFolders }";
-//    }
+    //    private readonly Task enumerateFoldersTask = Task.Run(() => EnumerateFolders(@"C:\", true));
+    //
+    //    /// <summary>
+    //    /// functionality to enumerate folders
+    //    /// <throws cref="DirectoryNotFoundException"></throws>
+    //    /// <throws cref="UnauthorizedAccessException"></throws>
+    //    /// <throws cref="PathTooLongException"></throws>
+    //    /// </summary>
+    //    /// 
+    //    public static string EnumerateFolders(string rootPath, bool enumerateChildFoldersIfAllowed = false)
+    //    {
+    //        try
+    //        {
+    //            foreach (var dirName in Directory.EnumerateDirectories(rootPath))
+    //            {
+    //                return dirName;
+    //                //Folders.Add(dirName);
+    //
+    //                //if (enumerateChildFoldersIfAllowed)
+    //                //{
+    //                //    EnumerateFolders(dirName, false);
+    //                //}
+    //            }
+    //        }
+    //        catch (DirectoryNotFoundException ex)
+    //        {
+    //            throw new DirectoryNotFoundException(rootPath, ex);
+    //        }
+    //        catch (UnauthorizedAccessException ex)
+    //        {
+    //            throw new UnauthorizedAccessException(rootPath, ex);
+    //        }
+    //        catch (PathTooLongException ex)
+    //        {
+    //            throw new PathTooLongException(rootPath, ex);
+    //        }
+    //
+    //        return "{ unknown error in $EnumerateFolders }";
+    //    }
 }
