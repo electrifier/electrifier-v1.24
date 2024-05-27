0918c09efe624e0e612753b985318666de528225
Thorsten Jung <tajbender@users.noreply.github.com>
2024-05-27 08:40:12 +0200

    Refactor Shell32GridView and Shell32GridViewItem classes
    Significant refactoring has been done to the `Shell32GridView.xaml` and `Shell32GridView.xaml.cs` files to improve readability and maintainability. The `Shell32GridView.xaml` file has been restructured with rearranged namespaces and added properties and event handlers to the `GridView` element. The `Shell32GridView.xaml.cs` file has seen the addition of the `System.Diagnostics` namespace, changes to the `Shell32GridView` class including the addition of the `DebuggerDisplay` attribute, modifications to properties and fields, and refactoring of methods.
    
    The `Shell32GridViewItem` class has also been extensively refactored with the addition of several properties, an updated constructor, and a new method to get the parent of a `ShellItem`. The `Shell32GridViewParentItem` method and the `Shell32GridViewItem` record have been removed. The `Microsoft.UI.Xaml.Media` and `Microsoft.UI.Xaml.Media.Imaging` namespaces have been added.

--- src/electrifier/Controls/Vanara/Shell32GridView.xaml.cs
+++ src/electrifier/Controls/Vanara/Shell32GridView.xaml.cs
@@ -15,183 +15,76 @@ using Vanara.PInvoke;
 using Vanara.Windows.Shell;
 using Windows.Foundation.Collections;
 using Windows.Foundation;
+using System.Diagnostics;
 
 namespace electrifier.Controls.Vanara;
 
+[DebuggerDisplay($"{{{nameof(GetDebuggerDisplay)}(),nq}}")]
 public sealed partial class Shell32GridView : UserControl
 {
-    public readonly ObservableCollection<Shell32GridViewItem> GridShellItems;
+    public ObservableCollection<Shell32GridViewItem> GridShellItems
+    {
+        get; private set;
+    }
 
-    private readonly ShellItem CurrentFolder;
+    private readonly ShellFolder CurrentFolder;
     public FolderItemFilter Filter { get; private set; } = FolderItemFilter.Folders | FolderItemFilter.NonFolders;
 
-    public Shell32GridView()
+    private readonly HWND windowHandle = default;
+
+
+    public Shell32GridView(/* hwnd */)
     {
         InitializeComponent();
         GridShellItems = new ObservableCollection<Shell32GridViewItem>();
-
-
         CurrentFolder = new ShellFolder(@"c:\");
-        //        GridShellItems.Add(new Shell32GridViewItem(ShellFolder.Desktop));
+        DataContext = this;
+
+        //ImageSource = new BitmapImage(new Uri("ms-appx:///Assets/StoreLogo.png"));
 
 
-
-        // make this async
-        Navigate(ShellFolder.Desktop, FolderItemFilter.Folders | FolderItemFilter.NonFolders);
-
-
-        //this.OnLoaded = (sender, e) =>
-        //{
-        //    Navigate(ShellFolder.Desktop);
-        //};
-
-
-
+        // TODO: make this async
+        GridShellItems = Navigate(CurrentFolder, FolderItemFilter.Folders | FolderItemFilter.NonFolders);
     }
 
-    public IEnumerable<Shell32GridViewItem> Navigate(ShellFolder folder, FolderItemFilter filter)
+    public ObservableCollection<Shell32GridViewItem> Navigate(ShellFolder folder, FolderItemFilter filter)
     {
-        if (CurrentFolder is null)
-        {
-            throw new ArgumentNullException(nameof(CurrentFolder));
-        }
+        var items = new ObservableCollection<Shell32GridViewItem>(Array.Empty<Shell32GridViewItem>());
 
-        if (CurrentFolder is not ShellFolder)
-        {
-            return Array.Empty<Shell32GridViewItem>();
-        }
-
-        var items = EnumerateItems(CurrentFolder, filter);
-
-        //: ((ShellFolder)CurrentFolder).EnumerateChildren(filter);
-
-
-        //var children = ((ShellFolder)CurrentFolder).EnumerateChildren(filter);
-        // 
-        if (CurrentFolder.Parent is not null)
-        {
-            var parent = Shell32GridViewItem.Shell32GridViewParentItem(CurrentFolder);
-            items.Add(parent);
-        }
-
-        // Shell32GridViewParentItem 
-        return items;
-    }
-
-    public List<Shell32GridViewItem> EnumerateItems(ShellItem shFolder, FolderItemFilter filter = (FolderItemFilter.Folders | FolderItemFilter.NonFolders))
-    {
-        var items = new List<Shell32GridViewItem>();
+        if (folder is null) { throw new ArgumentNullException(nameof(folder)); }
 
         try
         {
-            //if (shFolder is not ShellFolder)
-            //{
-            //    return Array.Empty<Shell32GridViewItem>();
-            //}
+            var parentItem = Shell32GridViewItem.Parent(folder);
 
-            //GridShellItems.Clear();
-            //CurrentFolder = shFolder;
+            if (parentItem is not null) { items.Add(parentItem); }
 
-            //foreach (var item in shFolder.EnumerateChildren())
-            //{
-            //    GridShellItems.Add(new Shell32GridViewItem(item));
-            //}
+            foreach (var item in folder.EnumerateChildren(filter, windowHandle))
+            {
+                items.Add(new Shell32GridViewItem(item));
+            }
         }
         catch (Exception)
         {
             throw;
         }
 
-        //GridShellItems.Clear();
-        //CurrentFolder = shFolder;
-
-        //if (CurrentFolder is not ShellFolder)
-        //{
-        //    return Array.Empty<Shell32GridViewItem>();
-        //}
-
-        //foreach (var item in shFolder.EnumerateChildren())
-        //{
-        //    GridShellItems.Add(new Shell32GridViewItem(item));
-        //}
         return items;
     }
 
-
-
-    void OnItemClick(object sender, ItemClickEventArgs e)
+    private void OnItemClickHandler(object _, ItemClickEventArgs e)
     {
         if (e.ClickedItem is Shell32GridViewItem item)
         {
-            if (item.ShellItem is ShellFolder folder)
+            if (item.ShellItem is ShellFolder folder /*&& folder.Parent is not null*/)
             {
-                Navigate(folder, this.Filter);
+                GridShellItems = Navigate(folder, Filter);
             }
         }
     }
-}
 
-
-
-
-
-/// <summary>
-/// Represents a single Item for the Shell32GridView
-/// </summary>
-/// <param name="shItem"></param>
-public record Shell32GridViewItem(ShellItem shItem)
-{
-    public string DisplayName
+    private string GetDebuggerDisplay()
     {
-        get; private set;
-    }
-
-    public bool IsFolder
-    {
-        get; private set;
-    }
-
-    public ShellItem ShellItem
-    {
-        get;
-    }
-
-    public Shell32GridViewItem() : this(ShellFolder.Desktop)
-    {
-        var shItem = ShellFolder.Desktop ?? throw new Exception("Desktop");
-        DisplayName = shItem.Name ?? throw new Exception(nameof(DisplayName));
-
-        //IsFolder = shItem is ShellFolder;
-        //ParsingName = shItem.ParsingName;
-    }
-
-
-    internal Shell32GridViewItem(ShellItem shItem, string displayName) : this(shItem)
-    {
-        DisplayName = displayName;
-        //ShellItem = shItem ?? throw new ArgumentNullException(nameof(shItem));
-        //DisplayName = shItem.Name;
-        //IsFolder = shItem is ShellFolder;
-        //ParsingName = shItem.ParsingName;
-    }
-
-    internal static Shell32GridViewItem Shell32GridViewParentItem(ShellItem shItem)
-    {
-        if (shItem is null)
-        {
-            throw new ArgumentNullException(nameof(shItem));
-        }
-
-        if (shItem.Parent is null)
-        {
-            throw new ArgumentNullException(nameof(shItem.Parent));
-        }
-
-        var newItem = new Shell32GridViewItem(shItem.Parent);
-        newItem.DisplayName = "Parent Folder";
-        newItem.IsFolder = true;
-        //ParsingName = shItem.Parent.ParsingName;
-
-        return newItem;
+        return nameof(Shell32GridView) + ToString();
     }
 }
---
+++ src/electrifier/Controls/Vanara/Shell32GridViewItem.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Media.Imaging;
+using Vanara.Windows.Shell;
+
+namespace electrifier.Controls.Vanara;
+
+public class /*record*/ Shell32GridViewItem
+{
+    public string DisplayName
+    {
+        get;
+    }
+
+    public string HintText
+    {
+        get; private set;
+    }
+
+    // TODO: Use: async bitmap for XAML binding: public readonly ImageSource ImageSource;
+
+    public bool IsFolder
+    {
+        get; private set;
+    }
+
+    public ShellItem ShellItem
+    {
+        get;
+    }
+
+    public Shell32GridViewItem(ShellItem shItem, string? overrideDisplayName = default)
+    {
+        ShellItem = shItem ?? throw new ArgumentNullException(nameof(shItem));
+        DisplayName = overrideDisplayName ?? (ShellItem.Name ?? throw new Exception("shItem Display Name"));
+        IsFolder = shItem is ShellFolder;
+        HintText = (IsFolder ? "Folder: " : "File: ") + shItem.ParsingName;
+
+        var bitmap = new BitmapImage
+        {
+            // TODO: make async: bitmap.SetSourceAsync("ms-appx:///...");
+            UriSource = new Uri("ms-appx:///Assets/Views/Workbench/Shell32 Default Folder.ico")
+        };
+    }
+
+    //public Shell32GridViewItem(ShellItem shItem, string displayName, string hintText, bool isFolder, ShellItem shellItem)
+    //    : this(shItem, displayName)
+    //{
+    //    HintText = hintText ?? throw new ArgumentNullException(nameof(hintText));
+    //    IsFolder = isFolder;
+    //    ShellItem = shellItem ?? throw new ArgumentNullException(nameof(shellItem));
+    //}
+
+    internal static Shell32GridViewItem? Parent(ShellItem shItem)
+    {
+        if (shItem is null)
+        {
+            throw new ArgumentNullException(nameof(shItem));
+        }
+
+        if (shItem.Parent is null)
+        {
+            return null;
+        }
+
+        return new Shell32GridViewItem(shItem.Parent, overrideDisplayName: "..")
+        {
+            IsFolder = true,
+            HintText = shItem.Parent.ParsingName ?? throw new Exception("shItem Display Name")
+        };
+    }
+}
