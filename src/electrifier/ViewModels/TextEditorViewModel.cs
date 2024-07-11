0401788d8b0c85fda45c50a5f51b9df130be9d61
Thorsten Jung <tajbender@users.noreply.github.com>
2024-07-11 14:38:33 +0200

    src: refactor

--- src/electrifier/ViewModels/TextEditorViewModel.cs
+++ src/electrifier/ViewModels/TextEditorViewModel.cs
@@ -1,17 +1,13 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
-using CommunityToolkit.WinUI.UI;
-using electrifier.Controls.Vanara;
 using Vanara.Windows.Shell;
 
 namespace electrifier.ViewModels;
 
 public partial class TextEditorViewModel : ObservableRecipient
 {
-    public ShellItem CurrentFolder;
+    public ShellItem CurrentFolder = ShellFolder.Desktop;
 
     public string CursorPosition => $"Column: {CursorX} Line: {CursorY}";
-    public int CursorX { get; set; } = 666;
+    public int CursorX { get; set; } = 0;
     public int CursorY { get; set; } = 0;
-
-    public TextEditorViewModel() => CurrentFolder = ShellFolder.Desktop;
 }
