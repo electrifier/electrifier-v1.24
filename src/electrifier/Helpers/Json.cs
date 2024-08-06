c75494760ee3e2b42478c6bf780c56c37500dd02
Thorsten Jung <tajbender@users.noreply.github.com>
2024-08-06 03:15:39 +0200

    doc

--- src/electrifier/Helpers/Json.cs
+++ src/electrifier/Helpers/Json.cs
@@ -1,22 +1,25 @@
 ﻿using Newtonsoft.Json;
+using static System.Threading.Tasks.Task;
 
 namespace electrifier.Helpers;
 
 public static class Json
 {
-    public static async Task<T> ToObjectAsync<T>(string value)
-    {
-        return await Task.Run(() =>
-        {
-            return JsonConvert.DeserializeObject<T>(value);
-        });
-    }
+    /// <summary>
+    /// ToObjectAsync&lt;T&gt;
+    /// </summary>
+    /// <typeparam name="T">Type of <see cref="object"/>.</typeparam>
+    /// <returns></returns>
+    public static async Task<T?> ToObjectAsync<T>(string value) =>
+        await Run(() => JsonConvert.DeserializeObject<T>(value));
 
+    /// <summary>
+    /// StringifyAsync(object value)
+    /// </summary>
+    /// <param name="value"></param>
+    /// <returns></returns>
     public static async Task<string> StringifyAsync(object value)
     {
-        return await Task.Run(() =>
-        {
-            return JsonConvert.SerializeObject(value);
-        });
+        return await Run(() => JsonConvert.SerializeObject(value));
     }
 }
