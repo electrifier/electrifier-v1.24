50ceac744df2a1831aa8617003d1975ee0e43d99
Thorsten Jung <tajbender@users.noreply.github.com>
2024-10-31 23:05:31 +0100

    gui fixes

--- src/electrifier/Controls/Vanara/Shell32BreadcrumbBar.xaml.cs
+++ src/electrifier/Controls/Vanara/Shell32BreadcrumbBar.xaml.cs
@@ -1,24 +1,25 @@
 using System.Collections.ObjectModel;
 using Microsoft.UI.Xaml.Controls;
 using Vanara.PInvoke;
-
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
-
 namespace electrifier.Controls.Vanara;
 public sealed partial class Shell32BreadcrumbBar : UserControl
 {
+    /// <summary><seealso href="https://learn.microsoft.com/en-us/windows/apps/design/controls/breadcrumbbar">
+    /// BreadcrumbBar control</seealso> for navigating through the Windows Shell32 namespace.</summary>
     public Shell32BreadcrumbBar()
     {
         InitializeComponent();
 
-        NativeBreadcrumbBar.ItemsSource = new ObservableCollection<Shell32.Folder> { };
+        NativeBreadcrumbBar.ItemsSource = new ObservableCollection<ExplorerBrowserItem> { };
         NativeBreadcrumbBar.ItemClicked += NativeBreadcrumbBar_ItemClicked;
     }
-
+    /// <summary><see cref="ItemClickEventHandler"/> for <see cref="Shell32BreadcrumbBar"/></summary>
+    /// <param name="sender">the owner of this event</param>
+    /// <param name="args">event args</param>
     private void NativeBreadcrumbBar_ItemClicked(BreadcrumbBar sender, BreadcrumbBarItemClickedEventArgs args)
     {
-        var items = NativeBreadcrumbBar.ItemsSource as ObservableCollection<Shell32.Folder>;
+        var items = NativeBreadcrumbBar.ItemsSource as ObservableCollection<ExplorerBrowserItem>;
+
         for (int i = items.Count - 1; i >= args.Index + 1; i--)
         {
             items.RemoveAt(i);
