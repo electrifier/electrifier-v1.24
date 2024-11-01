86dbca05911225be6f116c5941cdc74b0209b8d9
Thorsten Jung <tajbender@users.noreply.github.com>
2024-11-01 10:53:16 +0100

    gui: `TextEditorPage`

--- src/electrifier/ViewModels/TextEditorViewModel.cs
+++ src/electrifier/ViewModels/TextEditorViewModel.cs
@@ -1,12 +1,9 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using Vanara.Windows.Shell;
-
 namespace electrifier.ViewModels;
-
 public partial class TextEditorViewModel : ObservableRecipient
 {
     public ShellItem CurrentFolder = ShellFolder.Desktop;
-
     public string CursorPosition => $"Ln: {CursorY}   Ch: {CursorX}";
     public int CursorX { get; set; } = 0;
     public int CursorY { get; set; } = 0;
