60f54a836882f0e3c9143bdde9a5a7a27a6d32d8
Thorsten Jung <tajbender@users.noreply.github.com>
2024-11-11 12:44:44 +0100

    Add `IBrowserItem` Bump version to `v1.24.1111`

---
+++ src/electrifier/Controls/Vanara/Contracts/AbstractBrowserItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using Vanara.PInvoke;
+using Vanara.Windows.Shell;
+
+namespace electrifier.Controls.Vanara.Contracts;
+public class AbstractBrowserItem
+{
+    internal Shell32.IShellItemArray? GetChildArray(Shell32.SVGIO opt) => null;
+
+    public abstract class ABrowserItem(ShellItem shellItem) : ObservableObject
+    {
+        private readonly ShellItem _shellItem = shellItem;
+    }
+}
