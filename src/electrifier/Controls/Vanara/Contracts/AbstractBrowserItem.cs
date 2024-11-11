e87e4bdb08058e29052d4ad6990bdcd5dcd5b937
Thorsten Jung <tajbender@users.noreply.github.com>
2024-11-11 13:24:45 +0100

    prototyping: `AbstractBrowserItem`

--- src/electrifier/Controls/Vanara/Contracts/AbstractBrowserItem.cs
+++ src/electrifier/Controls/Vanara/Contracts/AbstractBrowserItem.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,13 +9,24 @@ using Vanara.PInvoke;
 using Vanara.Windows.Shell;
 
 namespace electrifier.Controls.Vanara.Contracts;
-public abstract class AbstractBrowserItem<T>(T shellItem)
+
+[DebuggerDisplay($"{{{nameof(ToString)}(),nq}}")]
+public abstract class AbstractBrowserItem<T>(T abstractItem)
 {
-    public T ShellItem { get; } = shellItem;
-    public Shell32.IShellItemArray? GetChildArray(Shell32.SVGIO opt) => null;
+    public T AbstractItem { get; } = abstractItem;
+
+    public ShellItem ShellItem = global::Vanara.Windows.Shell.ShellItem.Open(Shell32.PIDL.Null);
+    public Shell32.IShellItemArray GetChildArray(Shell32.SVGIO opt) => null;
+    public new string ToString() => $"AbstractBrowserItem<T {typeof(T)}>(T)";
 }
 
-public class KnownFolderBrowserItem(Shell32.KNOWNFOLDERID knownFolder)
-    : AbstractBrowserItem<ShellItem>(new ShellFolder(knownFolder))
-{
-}
+//public class KnownFolderBrowserItem(Shell32.KNOWNFOLDERID knownFolder)
+//    : AbstractBrowserItem<ShellItem>(new ShellFolder(knownFolder))
+//{
+//}
+
+//public class FolderBrowserItem(Shell32.Folder folder)
+//    : AbstractBrowserItem<ShellItem>(new ShellItem(folder)), Shell32.Folder
+//{
+
+//}
